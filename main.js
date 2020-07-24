233c27358182fa59c916fd61a87dc7e571a9ea60
Alejandro <arico@uc.cl>
2020-07-24 16:09:00 -0400

    Event Handling

--- main.js
+++ main.js
@@ -2,19 +2,29 @@ var app = new Vue ({
   el: "#app",
   data: {
     product: 'Socks',
-    image: "img/socks.jpg",
+    image: "img/vmSocks-green.jpg",
     inventory: 100,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
       {
         variantId: 2234,
-        variantColor: 'green'
+        variantColor: 'green',
+        variantImage: "./img/vmSocks-green.jpg"
       },
       {
         variantId: 2235,
-        variantColor: 'blue'
+        variantColor: 'blue',
+        variantImage: "./img/vmSocks-blue.jpg"
       }
     ],
-    cart: 0
-  }
+    cart: 0,
+  },
+    methods: {
+      addToCart() {
+        this.cart += 1
+      },
+      updateProduct(variantImage) {
+        this.image = variantImage
+      }
+    }
 })
